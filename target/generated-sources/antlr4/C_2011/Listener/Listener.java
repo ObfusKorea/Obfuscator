a402d62b323852a8b4468592f5bdd87bd27b0bb2
얌챠 <dbals9812@naver.com>
2020-04-27 01:02:01 +0900

    두 개 이상의 메소드에서 엔터 해결
    adder(){
    }/n<----이거 추가해줌
    adder2(){
    }

--- target/generated-sources/antlr4/C_2011/Listener/Listener.java
+++ target/generated-sources/antlr4/C_2011/Listener/Listener.java
@@ -1024,7 +1024,7 @@ public class Listener extends CBaseListener {
 	@Override
 	public void exitCompoundStatement(CParser.CompoundStatementContext ctx) {
 		String blocklist = (ctx.blockItemList() != null) ? newTexts.get(ctx.blockItemList()) : "";
-		String bf = String.format("{\n%s}", blocklist);
+		String bf = String.format("{\n%s}\n", blocklist);
 		newTexts.put(ctx, bf);
 	}
 
@@ -1199,7 +1199,7 @@ public class Listener extends CBaseListener {
 			bf = externDecl;
 		} else {
 			String transUnit = newTexts.get(ctx.translationUnit());
-			bf = String.format("%s %s", transUnit, externDecl);
+			bf = String.format("%s%s", transUnit, externDecl);
 		}
 		newTexts.put(ctx, bf);
 	}
