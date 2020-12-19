18f899a4110a9096dd60d8b03245ef63dcc960cc
yonniii <rkdtjdus753@gmail.com>
2020-12-19 17:18:25 +0900

    [VMOBFUS]Virtual 코드를 레지스터에 작성하기 위한 기능 구현

--- target/generated-sources/antlr4/C_2011/Listener/VM_Listener.java
+++ target/generated-sources/antlr4/C_2011/Listener/VM_Listener.java
@@ -1,7 +1,29 @@
 package C_2011.Listener;
 
 import listener.main.Obfuscator;
-public class VM_Listener extends Listener{
+public class VM_Listener extends Listener {
+    enum VCODE {
+        VPUSH, VADD, VSUB, VMULT, VASSGN
+    }
+
+    String getVCode(VCODE c) {
+        switch (c) {
+            case VPUSH:
+                return "VPUSH";
+            case VADD:
+                return "VADD";
+            case VSUB:
+                return "VSUB";
+            case VMULT:
+                return "VMULT";
+            case VASSGN:
+                return "VASSGN";
+            default:
+                return "NULL";
+        }
+    }
+
+    String add = "add(&encoded_bytes, %s);\n";
 
     public VM_Listener(int count) {
         super(count);
@@ -11,5 +33,9 @@ public class VM_Listener extends Listener{
     public String insertINIT(String input){
         String init = Obfuscator.getVMInit();
         return init+input;
+    }
+
+    String getAddCode(String s) {
+        return String.format(add, s);
     }
 }
